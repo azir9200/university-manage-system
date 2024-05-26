df6f3da915d615987c8cf26dbb7731f3a346fa4c
Azir@9200 <aziruddin83@hotmail.com>
2024-05-26 13:22:46 +0200

    created index fire for typescript project

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './modules/student/student.route';
+
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello Next!");
+});
+
+
+
+export default app;
